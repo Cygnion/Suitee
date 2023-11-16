272adb7419c2f19ea4b0cf9eb090babd1f3f763a
cyprien <cyprien.delapoeze-dharambure@etu.unicaen.fr>
2023-11-16 01:00:06 +0100

    🎨 navbar change test

--- source/Templates/navbar.php
+++ source/Templates/navbar.php
@@ -9,15 +9,24 @@
         </button>
         <nav class="NavBar" id="NavBar" style="border-bottom: 1px solid white;">
             <ul id="UlMenu">
-                <li><a class="Logo"><img src="assets/images/Logo.png" alt="Logo"></a></li>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="recipe.php">Nos Recettes</a></li>
+                <li><a class="Logo displayFex directionColumn">
+                    <img src="assets/images/Logo.png" alt="Logo">
+                </a></li>
+                <li><a class="displayFex directionColumn" href="index.php">
+                    Accueil
+                    <hr>
+                </a></li>
+                <li><a class="displayFex directionColumn" href="recipe.php">
+                    Nos Recettes
+                    <hr>
+                </a></li>
                 <li class="NavBarLiHasChildren">
                     <a>Filtres<span>▼</span></a>
+                    <hr>
                     <ul class="SubMenu">
-                        <li><a class="triggerCategory" href="#">Catégories</a></li>
-                        <li><a class="triggerTitle" href="#">Titre</a></li>
-                        <li><a class="triggerIngredient" href="#">Ingrédients</a></li>
+                        <li><a class="triggerCategory displayFex directionColumn" href="#">Catégories<hr></a></li>
+                        <li><a class="triggerTitle displayFex directionColumn" href="#">Titre<hr></a></li>
+                        <li><a class="triggerIngredient displayFex directionColumn" href="#">Ingrédients<hr></a></li>
                     </ul>
                 </li>
             </ul>
